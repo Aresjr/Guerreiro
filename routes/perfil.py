8981c43b599d5d20e64ae9f8043919f5582de8df
Ares <aristides.junor@philips.com>
2020-07-30 07:33:22 -0300

    General model changes

--- routes/perfil.py
+++ routes/perfil.py
@@ -7,7 +7,7 @@ from model.Usuario import Usuario
 
 @app.route('/perfil')
 def perfil():
-    cargo = Cargo(desc="Programador")
+    cargo = Cargo(titulo="Programador")
     usuario = Usuario(nome="Aristides Cândido Júnior", nickname="Ares", level=30, cargo=cargo,
                       email="aristidescandidojunior@gmail.com")
     return render_template('pages/perfil.html', title="Início", pageTitle="Início", usuario=usuario)
