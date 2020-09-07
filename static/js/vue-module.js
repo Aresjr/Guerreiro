ae8c5a53ce23bd7d78a21fcf8c8f823c1504d2f3
Ares <aristides.junor@philips.com>
2020-09-07 17:29:46 -0300

    Implementado Vue no template novo

--- static/js/vue-module.js
+++ static/js/vue-module.js
@@ -1,10 +1,14 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.min.js';
 Vue.use(VueRouter);
 
+import PaginaInicialComponent from '/static/js/components/pagina-inicial-component.js';
 import KanbanComponent from '/static/js/components/kanban-component.js';
 
 const router = new VueRouter({
   routes: [{
+    path: '/',
+    component: PaginaInicialComponent
+  }, {
     path: '/kanban',
     component: KanbanComponent
   }]
